4a47b98ca47641b6d3c367e8803b2bd171cea368
Milan van As <milanvanas@gmail.com>
2023-12-30 20:18:21 +0100

    More settings, templates, etc

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
-import {App, requestUrl, Editor, moment, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import {App, requestUrl, Editor, moment, Modal, Notice, Plugin, PluginSettingTab, Setting, ButtonComponent} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface OnThisDayPluginSettings {
-    accessToken: string;
-    amountOfEvents: number;
+    amountOfEvents: string;
     insertTitle: boolean;
     titleDateFormat: string;
+    titleTemplate: string;
+    itemTemplate: string;
 }
 
 interface OnThisDayItem {
@@ -16,8 +17,9 @@ interface OnThisDayItem {
 }
 
 const DEFAULT_SETTINGS: OnThisDayPluginSettings = {
-    accessToken: "",
-    amountOfEvents: 1,
+    titleTemplate: "## On this day ({{currentdate}})\n\n",
+    itemTemplate: "* {{eventdescription}} {{if eventyear}}({{eventyear}}){{endif}}\n",
+    amountOfEvents: "3",
     insertTitle: true,
     titleDateFormat: "MMMM Do"
 }
@@ -36,15 +38,7 @@ export default class OnThisDayPlugin extends Plugin {
             id: "insert-on-this-day-text-with-title",
             name: "Insert",
             editorCallback: async (editor: Editor) => {
-                await this.insert(editor, true);
-            },
-        });
-
-        this.addCommand({
-            id: "insert-on-this-day-text-without-title",
-            name: "Insert (without title)",
-            editorCallback: async (editor: Editor) => {
-                await this.insert(editor, false);
+                await this.insert(editor);
             },
         });
 
@@ -52,57 +46,71 @@ export default class OnThisDayPlugin extends Plugin {
 
     }
 
-    async insert(editor: Editor, withTitle = false) {
+    async insert(editor: Editor) {
 
-        // If the on this day response is empty, get it
-        if (Object.keys(this.onThisDayResponse).length === 0) {
+        if (!this.hasOnThisDayResponse()) {
 
             this.onThisDayResponse = await this.getOnThisDayResponse();
 
-            // If it's still empty, return
-            if (Object.keys(this.onThisDayResponse).length === 0) {
-                new Notice("Error getting on this day text");
+            if (!this.hasOnThisDayResponse()) {
+                new Notice("Error fetching text for today");
                 return;
             }
 
         }
 
+        const onThisDayText = this.getOnThisDayText();
 
         // If a selection is made, replace it with the text
         if (editor.somethingSelected()) {
-            editor.replaceSelection(this.getOnThisDayText(withTitle));
+            editor.replaceSelection(onThisDayText);
             return;
         }
 
+        const cursor = editor.getCursor();
+
         editor.replaceRange(
-            this.getOnThisDayText(withTitle),
-            editor.getCursor()
+            onThisDayText,
+            cursor
         );
 
-        editor.setCursor({line: editor.getCursor().line + 1, ch: 0});
     }
 
+    hasOnThisDayResponse(): boolean {
+        return this.onThisDayResponse.length > 0;
+    }
 
-    getOnThisDayText(withTitle: boolean): string {
+    getOnThisDayText(): string {
 
-        if (!this.onThisDayResponse) {
+        if (!this.hasOnThisDayResponse()) {
             return "";
         }
 
-        let text = "";
-
-        if (withTitle) {
-            text += `## On this day (${moment().format(this.settings.titleDateFormat)})\n\n`;
+        if (this.settings.itemTemplate === "") {
+            return "";
         }
 
-        // Add the events to the text
-        for (const event of this.onThisDayResponse.slice(0, this.settings.amountOfEvents)) {
-            text += `* ${event.eventdescription}`;
-            if (event.eventyear) {
-                text += ` (${event.eventyear})\n`;
-            } else {
-                text += `\n`;
+        let text = `${this.settings.titleTemplate}`;
+
+        text = text.replace('{{currentdate}}', moment().format(this.settings.titleDateFormat));
+
+        for (const event of this.onThisDayResponse.slice(0, parseInt(this.settings.amountOfEvents))) {
+
+            let eventText = `${this.settings.itemTemplate}`;
+
+            eventText = eventText.replace('{{eventdescription}}', event.eventdescription);
+
+            // Replace everything between {{if eventyear}} and {{endif}} if eventyear is not set
+            if (!event.eventyear) {
+                eventText = eventText.replace(/{{if eventyear}}([\s\S]*?){{endif}}/g, '');
             }
+
+            eventText = eventText.replace(/{{if eventyear}}/g, '');
+            eventText = eventText.replace(/{{endif}}/g, '');
+            eventText = eventText.replace('{{eventyear}}', event.eventyear.toString());
+
+
+            text += eventText;
         }
 
         return text;
@@ -166,30 +174,12 @@ export class OnThisDaySettingsTab extends PluginSettingTab {
         containerEl.empty();
 
         new Setting(containerEl)
-            .setName("Wikimedia Access token")
-            .setDesc("Get your Access token from https://api.wikimedia.org/wiki/Special:AppManagement")
-            .addTextArea((text) =>
-                text
-                    .setPlaceholder("Your Access token")
-                    .setValue(this.plugin.settings.accessToken)
-                    .onChange(async (value) => {
-
-                        this.plugin.settings.accessToken = value;
-                        await this.plugin.saveSettings();
-
-                        if (value && Object.keys(this.plugin.onThisDayResponse).length === 0) {
-                            this.plugin.onThisDayResponse = await this.plugin.getOnThisDayResponse();
-                        }
-                    })
-            );
-
-        new Setting(containerEl)
             .setName("Amount of items")
-            .setDesc("The amount of items to insert")
+            .setDesc("The amount of items to insert (max 10)")
             .addText((text) =>
                 text
-                    .setPlaceholder("Amount of items")
-                    .setValue(this.plugin.settings.amountOfEvents.toString())
+                    .setPlaceholder(DEFAULT_SETTINGS.amountOfEvents)
+                    .setValue(this.plugin.settings.amountOfEvents)
                     .onChange(async (value) => {
 
                         let parsedValue = parseInt(value);
@@ -202,8 +192,23 @@ export class OnThisDaySettingsTab extends PluginSettingTab {
                             parsedValue = 10;
                         }
 
-                        this.plugin.settings.amountOfEvents = parsedValue;
+                        this.plugin.settings.amountOfEvents = parsedValue.toString() === "NaN" ? DEFAULT_SETTINGS.amountOfEvents : parsedValue.toString();
 
+                        await this.plugin.saveSettings();
+                    })
+            );
+
+
+        new Setting(containerEl)
+            .setName("Title template")
+            .setDesc("The template that will be used for the title item that will be inserted. Use {{currentdate}} insert the formatted date (see the \"Title date format\" setting to specify the date format).")
+
+            .addText((textArea) =>
+                textArea
+                    .setPlaceholder(DEFAULT_SETTINGS.titleTemplate)
+                    .setValue(this.plugin.settings.titleTemplate.toString())
+                    .onChange(async (value) => {
+                        this.plugin.settings.titleTemplate = value === "" ? `${DEFAULT_SETTINGS.titleTemplate}` : value.trim() + "\n\n";
                         await this.plugin.saveSettings();
                     })
             );
@@ -214,7 +219,7 @@ export class OnThisDaySettingsTab extends PluginSettingTab {
             .setClass('title-date-format-setting')
             .addText((text) =>
                 text
-                    .setPlaceholder("Title date format (MMMM-Do)")
+                    .setPlaceholder(DEFAULT_SETTINGS.titleDateFormat)
                     .setValue(this.plugin.settings.titleDateFormat)
                     .onChange(async (value) => {
                         this.plugin.settings.titleDateFormat = value;
@@ -222,5 +227,73 @@ export class OnThisDaySettingsTab extends PluginSettingTab {
                     })
             );
 
+        new Setting(containerEl)
+            .setName("Item template")
+            .setDesc("The template that will be used for each item that will be inserted. Use {{eventdescription}} and {{eventyear}} to insert the values. Use {{if eventyear}} and {{endif}} to only show the year if it is set.")
+            .addText((textArea) =>
+                textArea
+                    .setPlaceholder(DEFAULT_SETTINGS.itemTemplate)
+                    .setValue(this.plugin.settings.itemTemplate.toString())
+                    .onChange(async (value) => {
+                        this.plugin.settings.itemTemplate = value === "" ? `${DEFAULT_SETTINGS.itemTemplate}` : value.trim() + "\n";
+                        await this.plugin.saveSettings();
+                    })
+            );
+
+
+        new ButtonComponent(containerEl)
+            .setButtonText("Reset settings to default")
+            .setWarning()
+            .setClass('reset-to-default-button')
+            .onClick(async () => {
+                new ResetSettingsToDefaultConfirmationModal(this.app, (reset) => {
+                    if (reset) {
+                        this.plugin.settings = Object.assign({}, DEFAULT_SETTINGS);
+                        this.display();
+                        this.plugin.saveSettings();
+                        new Notice("Settings reset to default (On this day plugin)");
+                    }
+                }).open();
+            });
+
+
     }
+}
+
+
+class ResetSettingsToDefaultConfirmationModal extends Modal {
+
+    onSubmit: (result: boolean) => void;
+
+    constructor(app: App, onSubmit: (result: boolean) => void) {
+        super(app);
+        this.onSubmit = onSubmit;
+    }
+
+    onOpen() {
+        const {contentEl} = this;
+
+        contentEl.createEl('h2', {text: 'Are you sure?'});
+
+        contentEl.createEl('p', {text: 'Are you sure you want to reset the settings to the default settings?'});
+
+        new ButtonComponent(contentEl)
+            .setButtonText("Reset settings to default")
+            .setClass('reset-to-default-button-confirm-button')
+            .setWarning()
+            .onClick(async () => {
+                this.onSubmit(true);
+                this.close();
+            });
+
+        new ButtonComponent(contentEl)
+            .setButtonText('Cancel')
+            .onClick(async () => {
+                this.onSubmit(false);
+                this.close();
+            });
+
+    }
+
+
 }
